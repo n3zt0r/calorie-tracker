f29255c28e28e774f54bdbc0e73242b5bdcb6f5e
Nestor Lopez <n3zt0r77@gmail.com>
2024-04-08 16:38:08 -0600

    Form created

---
+++ src/components/Form.tsx
@@ -0,0 +1,53 @@
+import { categories } from "../data/categories";
+
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category" className="font-bold">
+          Categoría:
+        </label>
+        <select
+          id="category"
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          id="activity"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorías:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorias Ej. 300 o 500"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="guardar comida o guardar ejercicio"
+      />
+    </form>
+  );
+}
