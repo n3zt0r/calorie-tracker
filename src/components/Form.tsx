65a5cbbbc78dac7dc0c266b419cbce10a3ac0490
Nestor Lopez <n3zt0r77@gmail.com>
2024-04-09 09:47:33 -0600

    Form state added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,27 @@
+import { useState, ChangeEvent } from "react";
+import type { Activity } from "../types";
 import { categories } from "../data/categories";
 
 export default function Form() {
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
+    console.log(isNumberField);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -10,6 +31,8 @@ export default function Form() {
         <select
           id="category"
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -20,14 +43,16 @@ export default function Form() {
       </div>
 
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
@@ -40,6 +65,8 @@ export default function Form() {
           type="number"
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias Ej. 300 o 500"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
 
