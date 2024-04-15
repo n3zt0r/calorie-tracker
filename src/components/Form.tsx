6e7a57ac4b0571f5844e25bc70a1c49a198d259e
Nestor Lopez <n3zt0r77@gmail.com>
2024-04-15 16:45:39 -0600

    Activities List added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
-import { ActivityActions } from "../reducers/activty-reducer";
+import { ActivityActions, ActivityState } from "../reducers/activty-reducer";
 
 type FormProps = {
   dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
 };
 
 const initialState: Activity = {
@@ -15,8 +16,23 @@ const initialState: Activity = {
   calories: 0,
 };
 
-export default function Form({ dispatch }: FormProps) {
+export default function Form({ dispatch, state }: FormProps) {
   const [activity, setActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      setActivity(selectedActivity);
+
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    }
+  }, [state.activeId]);
 
   const handleChange = (
     e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
--- src/reducers/activty-reducer.ts
+++ src/reducers/activty-reducer.ts
@@ -1,24 +1,60 @@
 import { Activity } from "../types";
 
-export type ActivityActions = {
-  type: "save-activity";
-  payload: { newActivity: Activity };
+export type ActivityActions =
+  | {
+      type: "save-activity";
+      payload: { newActivity: Activity };
+    }
+  | {
+      type: "set-activeId";
+      payload: { id: Activity["id"] };
+    }
+  | {
+      type: "delete-activity";
+      payload: { id: Activity["id"] };
+    };
+
+export type ActivityState = {
+  activities: Activity[];
+  activeId: Activity["id"];
 };
 
-type ActivityState = { activities: Activity[] };
-
-export const initialState: ActivityState = { activities: [] };
+export const initialState: ActivityState = { activities: [], activeId: "" };
 
 export const activityReducer = (
   state: ActivityState = initialState,
   action: ActivityActions
 ) => {
   if (action.type === "save-activity") {
-    //Este código maneja la lógica para manejar el state
+    let updatedActivities: Activity[] = [];
+    if (state.activeId) {
+      updatedActivities = state.activities.map((activity) =>
+        activity.id === state.activeId ? action.payload.newActivity : activity
+      );
+    } else {
+      updatedActivities = [...state.activities, action.payload.newActivity];
+    }
 
     return {
       ...state,
-      activities: [...state.activities, action.payload.newActivity],
+      activities: updatedActivities,
+      activeId: "",
+    };
+  }
+
+  if (action.type === "set-activeId") {
+    return {
+      ...state,
+      activeId: action.payload.id,
+    };
+  }
+
+  if (action.type === "delete-activity") {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
     };
   }
 
